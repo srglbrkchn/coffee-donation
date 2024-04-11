48bfd9509c4daff19c76612e25d95af41aa264b6
Saragol Barakchian <saragolbarakchian@wlan-e1428.fem.tu-ilmenau.de>
2024-04-11 13:15:51 +0200

    finish backbone of front-end, back-end and connection to stripe checkout. start the design.

---
+++ client/script.js
@@ -0,0 +1,30 @@
+const buttons = document.querySelectorAll(".coffee-btn");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    const itemId = parseInt(event.target.id);
+    fetch("http://localhost:3000/create-checkout-session", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        items: [{ id: itemId, quantity: 1 }],
+      }),
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        return res.json().then((json) => {
+          Promise.reject(json);
+        });
+      })
+      .then(({ url }) => {
+        window.location = url;
+      })
+      .catch((e) => {
+        console.error(e.error);
+      });
+  });
+});
